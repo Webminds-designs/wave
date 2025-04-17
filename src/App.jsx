2e52504531691938797fc5f530eb8418b539788e
SadiniWanniarachchi <sadiiw17@gmail.com>
2025-04-17 09:45:44 +0530

    feat-update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
-import ContactUs from "./pages/ContactUs";
+
 
 function App() {
   return (
@@ -10,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/features" element={<ContactUs />} />
 
       </Routes>
     </Router>
